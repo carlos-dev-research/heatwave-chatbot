9aaab709b903e5d9724e582e6a319e7dde6e1e7b
Carlos Gómez <carlosggleal@gmail.com>
2024-10-16 11:37:55 -0400

    fix of new conversation handling

--- models/session.py
+++ models/session.py
@@ -275,7 +275,7 @@ class session:
             chat_response = results[0][0][0]
             print(chat_response)
             if results[0][1][0]:
-                documents = json.loads(results[0][0][1])
+                documents = json.loads(results[0][1][0])
                 print(documents)
             else:
                 documents = [{}]
